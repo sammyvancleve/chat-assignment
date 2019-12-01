a3cb0dc986551c9fcf91320939b6f0cbe39cab3e
Sammy Van Cleve <svancleve@middlebury.edu>
2019-12-01 13:14:05 -0500

    add multithreading in client for receiving messages

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ chat-server: chat-server.c
 	gcc $(CFLAGS) $(LDFLAGS) -o chat-server chat-server.c
 
 chat-client: chat-client.c
-	gcc $(CFLAGS) -o chat-client chat-client.c
+	gcc $(CFLAGS) $(LDFLAGS) -o chat-client chat-client.c
 
 .PHONY: clean
 clean:
--- chat-client.c
+++ chat-client.c
@@ -3,6 +3,7 @@ chat-client.c
 */
 
 #include <stdlib.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,6 +12,8 @@ chat-client.c
 #include <string.h>
 
 #define BUF_SIZE 4096
+
+void *receive(void *conn_fd);
 
 int main(int argc, char *argv[]) {
     char *dest_hostname, *dest_port;
@@ -37,21 +40,26 @@ int main(int argc, char *argv[]) {
         exit(2);
     }
 
+    pthread_t rcvthread;
+    pthread_create(&rcvthread, NULL, receive, &conn_fd);
     //while loop until user exits (ctrl-d)
-    //maybe parse input from user before sending?
-    //ex for name change...
     while((n = read(0, buf, BUF_SIZE)) > 0) {
         send(conn_fd, buf, n, 0);
-
-        //todo separate thread for receiving info from server
-        n = recv(conn_fd, buf, BUF_SIZE, 0);
-        printf("received: ");
-        puts(buf);
     }
 
     //send disconnect before exiting
     strcpy(buf, "/disconnect");
-    send(conn_fd, buf, strlen("/disconnect", 0);
+    send(conn_fd, buf, BUF_SIZE, 0);
 
     close(conn_fd);
+}
+
+//TODO better condition for while loop (one that is actually useful...)
+void *receive(void *conn_fd) {
+    int fd = *((int *)conn_fd);
+    char buf[BUF_SIZE];
+    while(1) {
+        recv(fd, buf, BUF_SIZE, 0);
+        puts(buf);
+    }
 }
