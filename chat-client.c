dd568e0b7abe0b38f797e1304e43a4e9f2efce70
Sammy Van Cleve <svancleve@middlebury.edu>
2019-12-04 22:11:05 -0500

    client now outputs time string

--- chat-client.c
+++ chat-client.c
@@ -1,6 +1,6 @@
 /*
-chat-client.c
-*/
+   chat-client.c
+ */
 
 #include <stdlib.h>
 #include <pthread.h>
@@ -12,8 +12,10 @@ chat-client.c
 #include <string.h>
 
 #define BUF_SIZE 4096
+#define TIME_STRING_SIZE 15
 
 void *receive(void *conn_fd);
+void gettime(char *s);
 
 int main(int argc, char *argv[]) {
     char *dest_hostname, *dest_port;
@@ -47,7 +49,7 @@ int main(int argc, char *argv[]) {
         s = strtok(buf, "\n");
         send(conn_fd, s, n, 0);
     }
-    print("conn broken\n");
+    //printf("conn broken\n");
 
     //send disconnect before exiting
     strcpy(buf, "/disconnect");
@@ -60,9 +62,22 @@ void *receive(void *conn_fd) {
     int fd = *((int *)conn_fd);
     char buf[BUF_SIZE];
     while(recv(fd, buf, BUF_SIZE, 0) != 0) {
+        char s[TIME_STRING_SIZE];
+        gettime(s);
+        printf("%s", s);
         puts(buf);
         fflush(stdout);
     }
     printf("disconnected from server\n");
-    //connection broken
+    exit(0);
+}
+
+void gettime(char *s) {
+    time_t currenttime;
+    time(&currenttime);
+    struct tm *local = localtime(&currenttime);
+    int hr = local->tm_hour;
+    int min = local->tm_min;
+    int sec = local->tm_sec;
+    snprintf(s, TIME_STRING_SIZE, "%d:%d:%d: ", hr, min, sec);
 }
